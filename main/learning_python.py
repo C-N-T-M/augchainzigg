33b2acbd500ba8a792676db218ae1da901ad1cac
AugChainZigg <87276121+AugChainZigg@users.noreply.github.com>
2021-08-02 18:53:21 -0300

    Add files via upload

---
+++ main/learning_python.py
@@ -0,0 +1,27 @@
+import queue
+import time
+import requests
+import json
+import inspect
+import os
+import subprocess as sub
+import requests
+import datetime
+import pandas as pd
+
+"""
+Below there is two functions with commentaries.
+Function names start with lowcase letters, can't start with numbers, if a space is needed in the name you use '_' 
+"""
+
+def augchainzigg_function():
+    """AugChainZigg Learning space"""
+    print("This is augchainzigg learning space")
+    
+def tay_function():
+    """2DCrypto Learning space"""
+    print("This is Tay_2dcrypto learning space")
+
+if __name__ ==  "__main__" :
+    augchainzigg_function()
+    tay_function()
